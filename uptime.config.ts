3b23427b9a3dff189d39c45400ef7e710b183990
Kel <me@kelcuprum.ru>
2024-11-25 18:06:45 +0300

    Update configs

--- uptime.config.ts
+++ uptime.config.ts
@@ -17,61 +17,67 @@ const workerConfig = {
   monitors: [
     // Example HTTP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'foo_monitor',
-      // `name` is used at status page and callback message
-      name: 'My API Monitor',
-      // `method` should be a valid HTTP Method
-      method: 'POST',
-      // `target` is a valid URL
-      target: 'https://example.com',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://example.com',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      headers: {
-        'User-Agent': 'Uptimeflare',
-        Authorization: 'Bearer YOUR_TOKEN_HERE',
-      },
-      // [OPTIONAL] body to be sent
-      body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      checkLocationWorkerRoute: 'https://xxx.example.com',
+      id: 'site',
+      name: 'Main Site',
+      method: 'GET',
+      target: 'https://kelcuprum.ru',
     },
-    // Example TCP Monitor
     {
-      id: 'test_tcp_monitor',
-      name: 'Example TCP Monitor',
-      // `method` should be `TCP_PING` for tcp monitors
-      method: 'TCP_PING',
-      // `target` should be `host:port` for tcp monitors
-      target: '1.2.3.4:22',
-      tooltip: 'My production server SSH',
-      statusPageLink: 'https://example.com',
-      timeout: 5000,
+      id: 'api',
+      name: 'Alina API',
+      method: 'GET',
+      target: 'https://api.kelcuprum.ru',
     },
+    {
+      id: 'waterfiles',
+      name: 'WaterFiles',
+      method: 'GET',
+      target: 'https://wf.kelcu.ru',
+    },
+    {
+      id: 'repo',
+      name: 'Maven Repository',
+      method: 'GET',
+      target: 'https://repo.kelcuprum.ru/#',
+    },
+    {
+      id: 'waterplayer',
+      name: 'WaterPlayer Site',
+      method: 'GET',
+      target: 'https://waterplayer.ru',
+    },
+    {
+      id: 'waterplayer-api',
+      name: 'WaterPlayer API',
+      method: 'GET',
+      target: 'https://wplayer.ru',
+    },
+    {
+      id: 'pplmods',
+      name: 'PPL Mods',
+      method: 'GET',
+      target: 'https://pplmods.ru',
+    },
+    {
+      id: 'pplhelper',
+      name: 'PPL Helper Site',
+      method: 'GET',
+      target: 'https://h.pplmods.ru',
+    },
+    {
+      id: 'pplhelper-api',
+      name: 'PPL Helper API',
+      method: 'GET',
+      target: 'https://api-h.pplmods.ru',
+    },
+    {
+      id: 'red',
+      name: 'Redirects',
+      method: 'GET',
+      target: 'https://kelcu.ru/boo',
+    }
   ],
   notification: {
-    // [Optional] apprise API server URL
-    // if not specified, no notification will be sent
-    appriseApiServer: "https://apprise.example.com/notify",
-    // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
-    // if not specified, no notification will be sent
-    recipientUrl: "tgram://bottoken/ChatID",
-    // [Optional] timezone used in notification messages, default to "Etc/GMT"
-    timeZone: "Asia/Shanghai",
-    // [Optional] grace period in minutes before sending a notification
-    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-    // if not specified, notification will be sent immediately
-    gracePeriod: 5,
   },
   callbacks: {
     onStatusChange: async (
